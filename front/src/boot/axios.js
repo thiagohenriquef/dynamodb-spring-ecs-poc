a872e7915d16ee8eead6f9b9dea5e5d9a9c5789a
Thiago Henrique <thiagohferreira10@gmail.com>
2021-03-22 11:51:50 -0300

    Delete front directory

--- front/src/boot/axios.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-import axios from 'axios';
-
-Vue.prototype.$axios = axios.create({
-  baseURL: 'http://18.231.145.36:8081/api/',
-  timeout: 10000,
-});
--- front/src/i18n/index.js
+++
@@ -1,7 +0,0 @@
-import enUS from './en-us';
-import ptBR from './pt-br';
-
-export default {
-  'en-us': enUS,
-  'pt-br': ptBR,
-};
